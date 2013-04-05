7458df7c0f13b63fb883be59ddf7e32b796e64dc
Roy Storey <roy.storey@plantandfood.co.nz>
2013-04-06 08:44:58 +1300

    final message
    Signed-off-by: Roy Storey <roy.storey@plantandfood.co.nz>

--- import/build.sh
+++ import/build.sh
@@ -11,3 +11,7 @@ echo "[Build Log] BEGIN" > build.log
 cmake $CWD 2>&1 | tee -a build.log
 
 make       2>&1 | tee -a build.log
+
+echo ""                      >&2
+echo "Find import in build/" >&2
+echo ""                      >&2
