613ba2de8837eda5a72ed08fe1faa317814bb110
David Irvine <irvined@gmail.com>
2014-08-03 11:54:27 +0100

    Added documentation

---
+++ doc/index.rst
@@ -0,0 +1,26 @@
+.. LavaFlow documentation master file, created by
+   sphinx-quickstart on Sun Aug  3 11:38:20 2014.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to LavaFlow's documentation!
+====================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   install
+   import
+
+.. automodule:: lavaFlow
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- import/lava-import-openlava.py
+++ import/lava-import-openlava.py
@@ -21,61 +21,9 @@ import urllib2
 import json
 import argparse
 import time
-from openlava import lsblib, lslib
 import logging
-logging.basicConfig(level=logging.DEBUG)
-parser = argparse.ArgumentParser(description='Import OpenLava Log Files into LavaFlow')
-parser.add_argument('log_file', metavar='LOGFILE', type=str, help="Path to Logfile")
-parser.add_argument('url', metavar='URL', type=str, help="URL to LavaFlow server")
-parser.add_argument('key', metavar='KEY', type=str, help="Authentication key")
-parser.add_argument("--tail_log", action="store_true", default=False,
-                    help="When enabled, will not exit when the end of the input file is reached.  \
-                    Instead, it will wait for new data, or if the file is rotated, reopen the file \
-                    and continue reading.")
-parser.add_argument('--cluster_name', metavar="NAME", type=str,
-                    help="Optional cluster name to use, default is to get lsf cluster name", default=None)
-parser.add_argument('--chunk_size', metavar="CHUNK_SIZE", type=int, default=200, help="Number of records to group together before sending to server")
 
-args = parser.parse_args()
-
-# Open the event log file
-if not os.path.exists(args.log_file):
-    logging.critical("Error: Path: %s does not exist. Exiting.\n" % args.log_file)
-    sys.exit(1)
-try:
-    fh = open(args.log_file)
-except IOError as e:
-    logging.critical("Error: Unable to open %s: %s. Exiting.\n" % (args.log_file, str(e)))
-    sys.exit(1)
-
-# Get the cluster name
-if args.cluster_name:
-    cluster_name = args.cluster_name
-else:
-    cluster_name = lslib.ls_getclustername()
-if len(cluster_name) < 1:
-    logging.critical("Error: Unable to determine clustername. Exiting.\n")
-    sys.exit(2)
-
-# Set the URLs to submit to
-url = args.url.rstrip("/")
-url += "/clusters/%s/import/openlava" % cluster_name
-
-# Set the url to get the CSRF token
-token_url = args.url.rstrip("/")
-token_url += "/get_token"
-token = None
-try:
-    token_request = urllib2.Request(token_url)
-    f = urllib2.urlopen(token_request)
-    data = json.load(f)
-    if data['status'] != "OK":
-        logging.critical("Error: Unable to get CSRF Token: %s. Exiting\n" % data['message'])
-        sys.exit(1)
-    token=data['data']['cookie']
-except IOError as e:
-    logging.critical("Error: Unable to get CSRF Token: %s. Exiting\n" % str(e))
-    sys.exit(1)
+from openlava import lsblib, lslib
 
 
 def upload(rows):
@@ -86,7 +34,7 @@ def upload(rows):
     """
 
     # loop X times, then bail on error
-    request = urllib2.Request(url, json.dumps({'key':args.key, 'payload':rows}), {'Content-Type': 'application/json'})
+    request = urllib2.Request(url, json.dumps({'key': args.key, 'payload': rows}), {'Content-Type': 'application/json'})
     # Ensure server knows this is an AJAX request.
     request.add_header('HTTP_X_REQUESTED_WITH', 'XMLHttpRequest')
     request.add_header('X-Requested-With', 'XMLHttpRequest')
@@ -99,7 +47,7 @@ def upload(rows):
         count += 1
         try:
             f = urllib2.urlopen(request)
-            data=json.load(f)
+            data = json.load(f)
             if data['status'] == "OK":
                 logging.info("Imported %d rows." % row_num)
                 failed = False
@@ -116,6 +64,7 @@ class OLDumper(json.JSONEncoder):
     """
     Encoder to dump OpenLava objects to JSON.
     """
+
     def default(self, obj):
         """
         Attempts to call __to_dict(), the method used to return a dictionary copy, on the supplied object.
@@ -130,6 +79,78 @@ class OLDumper(json.JSONEncoder):
             return json.JSONEncoder.default(self, obj)
 
 
+parser = argparse.ArgumentParser(description='Import OpenLava Log Files into LavaFlow')
+parser.add_argument('log_file', metavar='LOGFILE', type=str, help="Path to Logfile")
+parser.add_argument('url', metavar='URL', type=str, help="URL to LavaFlow server")
+parser.add_argument('key', metavar='KEY', type=str, help="Authentication key")
+parser.add_argument("--tail_log", action="store_true", default=False,
+                    help="When enabled, will not exit when the end of the input file is reached.  \
+                    Instead, it will wait for new data, or if the file is rotated, reopen the file \
+                    and continue reading.")
+parser.add_argument('--cluster_name', metavar="NAME", type=str,
+                    help="Optional cluster name to use, default is to get lsf cluster name", default=None)
+parser.add_argument('--chunk_size', metavar="CHUNK_SIZE", type=int, default=200,
+                    help="Number of records to group together before sending to server")
+parser.add_argument('--log_level', metavar="LOG_LEVEL", type=str, default="warn", help="Log level to use")
+parser.add_argument("--retry_forever", action="store_true", default="False",
+                    help="Keep trying, even when server responds with an error")
+
+
+args = parser.parse_args()
+
+numeric_level = getattr(logging, args.log_level.upper(), None)
+if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % args.log_level)
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=numeric_level)
+
+
+# Open the event log file
+if not os.path.exists(args.log_file):
+    logging.critical("Error: Path: %s does not exist. Exiting.\n" % args.log_file)
+    sys.exit(1)
+try:
+    fh = open(args.log_file)
+except IOError as e:
+    logging.critical("Error: Unable to open %s: %s. Exiting.\n" % (args.log_file, str(e)))
+    sys.exit(1)
+
+
+# Get the cluster name
+if args.cluster_name:
+    cluster_name = args.cluster_name
+else:
+    cluster_name = lslib.ls_getclustername()
+if len(cluster_name) < 1:
+    logging.critical("Error: Unable to determine clustername. Exiting.\n")
+    sys.exit(2)
+logging.info("My cluster name is: %s" % cluster_name)
+
+
+# Set the URLs to submit to
+url = args.url.rstrip("/")
+url += "/clusters/%s/import/openlava" % cluster_name
+logging.debug("My upload URL is: %s" % url)
+
+
+# Set the url to get the CSRF token
+token_url = args.url.rstrip("/")
+token_url += "/get_token"
+logging.debug("My token URL is: %s" % token_url)
+token = None
+try:
+    token_request = urllib2.Request(token_url)
+    f = urllib2.urlopen(token_request)
+    data = json.load(f)
+    if data['status'] != "OK":
+        logging.critical("Error: Unable to get CSRF Token: %s. Exiting\n" % data['message'])
+        sys.exit(1)
+    token = data['data']['cookie']
+    logging.debug("Got token: %s" % token)
+except IOError as e:
+    logging.critical("Error: Unable to get CSRF Token: %s. Exiting\n" % str(e))
+    sys.exit(1)
+
+
 # Iterate through the log file and upload in batches of 200
 row_num = 0
 rows = []
@@ -138,8 +159,23 @@ while True:
     if rec is None:
         if lsblib.get_lsberrno() == lsblib.LSBE_EOF:
             logging.info("EOF Reached, waiting on new data")
-            time.sleep(20)
-            continue
+            if not os.path.exists(args.log_file):
+                logging.warn("Log file no longer exists, waiting on new one being created...")
+                time.sleep(60)
+                continue
+            if os.fstat(fh).st_ino != os.stat(args.log_file).st_ino:
+                logging.info("Log file has been rotated, opening new file...")
+                fh.close()
+                try:
+                    fh = open(args.log_file)
+                    continue
+                except IOError as e:
+                    logging.critical("Error: Unable to open %s: %s. Exiting.\n" % (args.log_file, str(e)))
+                    sys.exit(1)
+            else:
+                time.sleep(60)
+                continue
+
     if lsblib.get_lsberrno() == lsblib.LSBE_EVENT_FORMAT:
         logging.error("Bad Row: %s in %s" % (row_num, args.log_file))
         continue
