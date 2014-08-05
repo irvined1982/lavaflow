fe03fd66cc570d2f7656714501b3f8e50d63c61f
David Irvine <irvined@gmail.com>
2014-08-05 11:20:47 +0100

    Improved util view

--- lavaFlow/views.py
+++ lavaFlow/views.py
@@ -950,6 +950,9 @@ def consumption_bar_data(request, start_time_js=0, end_time_js=0, exclude_string
                 }
             }
         print data
+        d=data[group_name]
+        d=d['values']
+        d=d['Sum CPU']
         data[group_name]['values']['Sum CPU'] += row['cpu_time__sum']
         data[group_name]['values']['Sum Wall'] += row['wall_time__sum']
         data[group_name]['values']['Sum Pend'] += row['pend_time__sum']
