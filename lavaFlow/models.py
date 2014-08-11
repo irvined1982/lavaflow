e0ca09350225725c9bfed543e7d12391c39d3c8d
David Irvine <irvined@gmail.com>
2014-08-11 12:58:24 +0100

    Experiment with filter list building

--- lavaFlow/models.py
+++ lavaFlow/models.py
@@ -1387,7 +1387,6 @@ class Attempt(models.Model):
             ['start_time','end_time',],
             ['start_time','end_time','num_processors','cluster'],
             ['start_time','end_time','num_processors','user'],
-
             ['start_time','end_time','num_processors','queue'],
             ['start_time','end_time','num_processors'],
             ['cluster', 'job', 'task'],
